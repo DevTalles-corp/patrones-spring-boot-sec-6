24679a9816cd2c0b141159756f678b42f251831b
Gabriel Chaldu <gchaldu@MacBook-Pro-de-Gabriel.local>
2026-05-02 12:45:45 -0300

    final sección 6

--- src/main/java/com/atlas/bank/atlas_bank/transaction/dto/TransactionMapper.java
+++ src/main/java/com/atlas/bank/atlas_bank/transaction/dto/TransactionMapper.java
@@ -3,6 +3,7 @@ package com.atlas.bank.atlas_bank.transaction.dto;
 import com.atlas.bank.atlas_bank.transaction.model.Transaction;
 import org.mapstruct.Mapper;
 
+//Mapper
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
     TransactionResponse toResponse(Transaction transaction);
